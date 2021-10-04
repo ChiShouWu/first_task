f72c56e1339c3a72b4d4f10e3c1645e3550cbcac
ChiShouWu <u0151033@nkfust.edu.tw>
2021-10-04 17:19:48 +0800

    feat: init project and create basic user api

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, Post, Req, Res } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) { }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Post('world')
+  getWorld(@Req() request): { name: string, val: number } {
+    console.log(request);
+    return { name: 'world',val: 123};
+  }
+}
---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,5 @@
+export class CreateUserDto {
+  account: string;
+  password: string;
+  username: string;
+}
